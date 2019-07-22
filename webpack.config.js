1caa2a93783e8ad72b73ad5f359830819bedfc49
pwrmvspark <danielparkonline@gmail.com>
2019-07-21 18:16:17 -0700

    speed optimization #1 installed newer version of webpack which includes compression

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,50 @@
-var path = require('path');
-var SRC_DIR = path.join(__dirname, '/client/src');
-var DIST_DIR = path.join(__dirname, '/client/dist');
+// var path = require('path');
+// var SRC_DIR = path.join(__dirname, '/client/src');
+// var DIST_DIR = path.join(__dirname, '/client/dist');
+
+// module.exports = {
+//   entry: `${SRC_DIR}/index.jsx`,
+//   output: {
+//     filename: 'bundle.js',
+//     path: DIST_DIR
+//   },
+//   module: {
+//     loaders: [
+//       {
+//         test: /\.jsx?/,
+//         include: SRC_DIR,
+//         loader: 'babel-loader',
+//         query: {
+//           presets: ['react', 'es2015']
+//         }
+//       },
+//       {
+//         test: /\.css/,
+//         loader: ['style-loader', 'css-loader?modules']
+//       }
+//     ]
+//   }
+// };
+
+var path = require("path");var 
+SRC_DIR = path.join(__dirname, '/client/src');
+var DIST_DIR = path.join(__dirname, "/client/dist");
 
 module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
   output: {
-    filename: 'bundle.js',
-    path: DIST_DIR
+    filename: "bundle.js",
+    path: DIST_DIR,
+    publicPath: '/'
   },
   module: {
-    loaders: [
+    rules: [
       {
-        test: /\.jsx?/,
-        include: SRC_DIR,
-        loader: 'babel-loader',
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
         query: {
-          presets: ['react', 'es2015']
+          presets: ["@babel/preset-env", "@babel/preset-react"]
         }
       },
       {
